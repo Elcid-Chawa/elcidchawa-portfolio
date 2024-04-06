b26885d1fad318d224460583c5fc1f3c4db11c10
Elcid-Chawa <elcidchawa@gmail.com>
2024-04-06 17:26:31 +0200

    fixed responsiveness

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -6,9 +6,10 @@ interface Props {
   image: string;
   title: string;
   text: string;
+  url?: string;
 }
 
-const ProjectCard = ({ image, title, text }: Props) => {
+const ProjectCard = ({ image, title, text, url }: Props) => {
   const [isFlipped, setIsFlipped] = useState(false);
   const [isAnimating, setIsAnimating] = useState(false);
 
@@ -21,7 +22,7 @@ const ProjectCard = ({ image, title, text }: Props) => {
   return (
     <div
       onClick={handleFlip}
-      className="w-[450px] h-[280px] rounded-md cursor-pointer"
+      className="w-[80vw] md:w-[40vw] md:h-[40vh] h-[35vh] rounded-md cursor-pointer"
     >
       <motion.div
         className="flip-card-inner w-full h-full"
@@ -32,7 +33,7 @@ const ProjectCard = ({ image, title, text }: Props) => {
       >
         <div
           style={{ backgroundImage: `url(${image})` }}
-          className="w-full h-full group relative flip-card-front bg-cover bg-center text-white rounded-lg p-4"
+          className="w-full h-full group relative flip-card-front bg-cover text-white rounded-lg p-4"
         >
           <div className="absolute inset-0 w-full h-full rounded-md bg-black opacity-0 group-hover:opacity-40" />
           <div className="absolute inset-0 w-full h-full text-[20px] pb-10 hidden group-hover:flex items-center z-[20] justify-center">
@@ -41,12 +42,18 @@ const ProjectCard = ({ image, title, text }: Props) => {
         </div>
         <div
           style={{ backgroundImage: `url(${image})` }}
-          className="w-full h-full group relative flip-card-back bg-cover bg-center text-white rounded-lg p-4"
+          className="w-full h-full group relative flip-card-back bg-cover  text-white rounded-lg p-4"
         >
-          <div className="absolute inset-0 w-full h-full rounded-md bg-black opacity-50 z-[-1]" />
-          <div className="flex flex-col gap-20 py-3 z-[30] hover:backdrop-blur-sm">
-            <h1 className="text-whote text-2xl font-semibold">{title}</h1>
-            <p className="text-gray-200 text-[20px]">{text}</p>
+          <div className="absolute inset-0 w-full h-full rounded-md bg-black opacity-95 z-[-1]" />
+          <div className="flex flex-col h-full w-auto gap-3 md:gap-10 py-3 z-[30]">
+            <h1 className="text-whote text-xl font-semibold">{title}</h1>
+            <p className="text-gray-200 md:text-[18px] ">{text}</p>
+            <p className="text-gray-200 md:text-[16px] text-[14px] text-wrap">
+              Project URL:{" "}
+              <a href={url} target="_blank">
+                {url}
+              </a>
+            </p>
           </div>
         </div>
       </motion.div>
